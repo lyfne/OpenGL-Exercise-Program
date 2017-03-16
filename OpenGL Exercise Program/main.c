6b10a3189722b55a9d061ef61d6df04ac11f1a66
IvanLin <lyfne@me.com>
2017-03-16 12:07:53 +0800

    Import glfw library and first run

--- OpenGL Exercise Program/main.c
+++ OpenGL Exercise Program/main.c
@@ -6,10 +6,40 @@
 //  Copyright © 2017年 Ivan.lin. All rights reserved.
 //
 
-#include <stdio.h>
+#include <GLFW/glfw3.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void)
+{
+    GLFWwindow* window;
+    
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+    
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1024, 768, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+    
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+    
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+        
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
 }
