8466e6335f0ebcbc0e217c95f082a929b4d67d4b
IvanLin <lyfne@me.com>
2017-03-16 10:35:23 +0800

    Initial Commit

---
+++ OpenGL Exercise Program/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  OpenGL Exercise Program
+//
+//  Created by 林逸凡 on 2017/3/16.
+//  Copyright © 2017年 Ivan.lin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
